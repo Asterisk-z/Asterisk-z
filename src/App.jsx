32a9460de6e923757971a682d5cd5089a38d42f2
Olang Daniel <olangdan17@gmail.com>
2022-04-12 18:46:40 +0100

    Setup

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Header from './components/header/Header';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
+import Testimonials from './components/testimonials/Testimonials';
+import Services from './components/services/Services';
+import Portfolio from './components/portfolio/Portfolio';
+import Experience from './components/experience/Experience';
+import Navigation from './components/navigation/Navigation';
+
+const App = () => {
+  return (
+      <>
+        <Header />
+        <Navigation />
+        <About />
+        <Contact />
+      </>
+  )
+}
+
+export default App
