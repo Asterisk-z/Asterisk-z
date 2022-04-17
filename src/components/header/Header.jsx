8f28fb642adab83fd458c7557af3882b0cb50fe0
Olang Daniel <olangdan17@gmail.com>
2022-04-17 18:52:25 +0100

    added content

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import CTA from './CTA';
 import HeaderSocials from './HeaderSocials';
-import ME from '../../assets/me.png';
+import Laravel from '../../assets/laravel.png';
+import Html from '../../assets/html.png';
+import Css from '../../assets/css.png';
+import Vue from '../../assets/vue.png';
+import node from '../../assets/node.png';
+import react from '../../assets/react.png';
+import database from '../../assets/database.jpg';
+import php from '../../assets/php.png';
+// import ME from '../../assets/me.png';
+// import ME1 from '../../assets/profile.jpeg';
+// import ME2 from '../../assets/profile-0.png';
 import "./header.css";
 
 
@@ -10,24 +20,45 @@ const Header = () => {
     
     <header>
       
+        <div className='overlay'>
         <div className='container header__container'>
-            <h5>Hello I'm</h5>
-            <h1>Olang Daniel</h1>
-            <h5 className='text-light'>Fullstack Developer</h5>
-            <CTA />
+            <div className='action'>
+              <h5>Hello I'm</h5>
+              <h1>Olang Daniel</h1>
+              <h5 className='text-light'>Fullstack Developer</h5>
+              <CTA />
+            </div>
             <HeaderSocials />
             <div className='me-0'>
-              <img src={ME} alt='me' />
+              <img src={Laravel} alt='me' />
             </div>
             <div className='me-1'>
-              <img src={ME} alt='me' />
+              <img src={Html} alt='me' />
             </div>
-            <div className='me-2'>
-              <img src={ME} alt='me' />
+            {/* <div className='me-2'>
+              <img src={ME2} alt='me' />
+            </div> */}
+            <div className='me-3'>
+              <img src={Css} alt='me' />
+            </div>
+            <div className='me-4'>
+              <img src={Vue} alt='me' />
+            </div>
+            <div className='me-5'>
+              <img src={react} alt='me' />
+            </div>
+            <div className='me-6'>
+              <img src={node} alt='me' />
+            </div>
+            <div className='me-7'>
+              <img src={database} alt='me' />
+            </div>
+            <div className='me-8'>
+              <img src={php} alt='me' />
             </div>
 
             <a href='#contact' className='scroll__down'>Scroll Down</a>
-
+          </div>
         </div>
       </header>
   )
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,71 @@
 import React from 'react';
 import "./testimonials.css";
+import Avatar1 from '../../assets/avatar1.jpg';
+import Avatar2 from '../../assets/avatar2.jpg';
+import Avatar3 from '../../assets/avatar3.jpg';
+import Avatar4 from '../../assets/avatar4.jpg';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const data = [
+  {
+    avatar: Avatar1,
+    name: 'Tina Snow',
+    review: 'lorem jefie fjerfi ufdsn fjdfsdfn fdsmfbyhi jdsfhsh sdifuhfn frbrfb fsdifuref iuffnfnurfg freufrefb ferfydfh erfierufgb rfyurgfrb fyrefuire fuyeufbi jfwuefb'
+  },
+  {
+    avatar: Avatar4,
+    name: 'Tina Snow',
+    review: 'lorem jefie fjerfi ufdsn fjdfsdfn fdsmfbyhi jdsfhsh sdifuhfn frbrfb fsdifuref iuffnfnurfg freufrefb ferfydfh erfierufgb rfyurgfrb fyrefuire fuyeufbi jfwuefb'
+  },
+  {
+    avatar: Avatar2,
+    name: 'Tina Snow',
+    review: 'lorem jefie fjerfi ufdsn fjdfsdfn fdsmfbyhi jdsfhsh sdifuhfn frbrfb fsdifuref iuffnfnurfg freufrefb ferfydfh erfierufgb rfyurgfrb fyrefuire fuyeufbi jfwuefb'
+  },
+  {
+    avatar: Avatar3,
+    name: 'Tina Snow',
+    review: 'lorem jefie fjerfi ufdsn fjdfsdfn fdsmfbyhi jdsfhsh sdifuhfn frbrfb fsdifuref iuffnfnurfg freufrefb ferfydfh erfierufgb rfyurgfrb fyrefuire fuyeufbi jfwuefb'
+  },
+];
 
 const Testimonials = () => {
   return (
-    <section id="testimonial">Testimonials</section>
+    <section id="testimonial">
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper className='container testimonial__container'
+      // install Swiper modules
+      modules={[ Pagination, ]}
+      spaceBetween={40}
+      slidesPerView={1.1}
+      pagination={{ clickable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+      >
+        {
+          data.map(({ avatar, name, review }, index) => {
+            return (
+              <SwiperSlide className='testimonial' key={index}>
+                <div className='client__avatar'>
+                  <img src={ avatar } alt='' />
+                </div>
+                  <h5 className='client__name'>{ name }</h5>
+                  <small className='client_review'>
+                    { review }
+                  </small>
+              </SwiperSlide>
+            )
+            
+          })
+        }
+
+      </Swiper>
+    </section>
   )
 }
 
